23d0c97e1edf99282cf8e4e7779111569958e15f
Eman Hosni <Eman.Hosni@afatechnologies.net>
2021-07-25 00:24:48 +0200

    Add project files.

---
+++ GalleryApp/GalleryApp/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using GalleryApp.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GalleryApp
+{
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Detail = new NavigationPage(new ArticlesPage());
+        }
+
+        private void OpenLiveChatPage(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new LiveChatPage());
+            IsPresented = false;
+        }
+
+        private void OpenGalleryPage(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new GalleryPage());
+            IsPresented = false;
+
+        }
+
+        private void OpenWishListPage(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new WishListPage());
+            IsPresented = false;
+        }
+
+        private void OpenOnlineNewsPage(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new OnlineNewsPage());
+            IsPresented = false;
+
+        }
+
+        private void OpenArticlesPage(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new ArticlesPage());
+            IsPresented = false;
+        }
+    }
+}
